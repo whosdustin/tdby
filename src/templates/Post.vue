751dfb7aa713ca7ebdc19771cd883d551c258cab
Dustin Delatore <dustin@delatore.me>
2019-07-29 22:29:04 -0400

    fix: I think I got it working

---
+++ src/templates/Post.vue
@@ -0,0 +1,39 @@
+<template>
+  <Layout>
+    <g-link to="/" class="link">  &larr; Go Back</g-link>
+    <div class="post-title">
+      <h1>{{$page.post.title}}</h1>
+      <p class="post-date"> {{ $page.post.date}} | {{$page.post.timeToRead}} min read</p>
+    </div>
+    <div class="post-content">
+      <p v-html="$page.post.content" />
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query Post ($path: String!) {
+  post: post (path: $path) {
+    id
+    title
+    content
+    date (format: "D MMMM YYYY")
+    timeToRead
+  }
+}
+</page-query>
+
+<script>
+import Layout from '~/layouts/Default.vue';
+
+export default {
+  components: {
+    Layout
+  },
+  metaInfo() {
+    return {
+      title: this.$page.post.title
+    }
+  }
+}
+</script>
